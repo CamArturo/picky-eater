efc4006630999677fe8de6b939a3fd52e29281cc
Camilo Snapp <camilo.snapp@gmail.com>
2018-05-28 15:08:49 -0600

    Added Hero

---
+++ src/components/stateless/hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <section className="hero-container">
+      <section className="inner">
+        <h2>Find it. Finally.</h2>
+        <button>Restaurant Match</button>
+      </section>
+    </section>
+  );
+};
+
+export default Hero;
