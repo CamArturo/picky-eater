7c9870a5cf0295fe5c4358f2e7f78a75c901b2cf
Camilo Snapp <camilo.snapp@gmail.com>
2018-05-28 21:56:12 -0600

    Add api fetch foundation

--- src/components/stateless/location/Location.js
+++ src/components/stateless/location/Location.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Location.css';
 import LocationInput from '../locationInput/LocationInput';
+import Locations from '../locations/Locations';
 
 const Location = () => {
   return (
     <section className="location-container">
       <LocationInput />
-      
+      <Locations/>
     </section>
   );
 };
