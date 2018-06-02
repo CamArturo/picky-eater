c4d882a40f97f1404859da5c2882a1d8a847d464
Camilo Snapp <camilo.snapp@gmail.com>
2018-06-02 11:49:10 -0600

    Add Cuisines action creator and reducer

---
+++ src/reducers/cuisineReducer.js
@@ -0,0 +1,8 @@
+export const availableCuisines = (state = {}, action) => {
+  switch (action.type) {
+    case 'ADD_AVAILABLE_CUISINES':
+      return action.cuisines;
+    default:
+      return state
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import { locations } from './locationReducers';
 import { chosenCity } from './chosenCityReducer';
+import { cuisines} from './cuisineReducer';
 
 const rootReducer = combineReducers(
   {
     locations,
-    chosenCity
+    chosenCity,
+    cuisines
   }
 );
 
