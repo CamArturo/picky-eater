aec168bf725526925b1e7fd4961a62ef0a3962db
Camilo Snapp <camilo.snapp@gmail.com>
2018-06-02 11:33:28 -0600

    Added Testing for Locations.js

--- src/components/stateless/locations/Locations.js
+++ src/components/stateless/locations/Locations.js
@@ -7,7 +7,7 @@ export class Locations extends Component {
   displayLocations = () => {
     if (this.props.locations.length > 0) {
       return this.props.locations.map((location, index) => (
-        <button key={`key${index}`} onClick={() => this.props.storeChosenCity(location.name)}>{location.name}</button>
+        <button className="location-btn" key={`key${index}`} onClick={() => this.props.storeChosenCity(location.name)}>{location.name}</button>
       ));
     }
   };
@@ -17,7 +17,7 @@ export class Locations extends Component {
       <section className="locations-container">
         {this.displayLocations()}
       </section>
-    );
+    )
   }
 }
 
