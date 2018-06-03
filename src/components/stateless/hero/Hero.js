13721aae0545413e3978e4280dfe2243aa520e29
Camilo Snapp <camilo.snapp@gmail.com>
2018-06-03 12:05:51 -0600

    Added CSS and wording for the homepage

--- src/components/stateless/hero/Hero.js
+++ src/components/stateless/hero/Hero.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
-import { connect } from 'react-redux';
 import { NavLink, Route, withRouter } from 'react-router-dom';
 import './Hero.css';
 
